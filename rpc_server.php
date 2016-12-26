6872893266fa724fcf7c205921221ecd0cf4f154
fucongcong <fucongcong@clothesmake.com>
2016-12-26 15:16:02 +0800

    fix rpc

--- rpc_server.php
+++ rpc_server.php
@@ -1,7 +1,7 @@
 <?php
 
 define('__ROOT__', realpath(dirname(__FILE__)) . DIRECTORY_SEPARATOR);
-require_once __ROOT__.'vendor/hprose/hprose/src/Hprose.php';
+require_once __ROOT__.'core/Group/Plugin/hprose/src/Hprose.php';
 require_once __ROOT__.'core/Group/RpcKernal.php';
 
 $kernal = new RpcKernal('tcp');
