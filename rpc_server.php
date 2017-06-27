04abe41b4a83f4fe99169c4d5ad2f374de735cc1
fucongcong <fucongcong@clothesmake.com>
2017-06-27 17:26:55 +0800

    rm rpc

--- rpc_server.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('__ROOT__', realpath(dirname(__FILE__)) . DIRECTORY_SEPARATOR);
-require_once __ROOT__.'core/Group/Plugin/Rpc/Hprose.php';
-require_once __ROOT__.'core/Group/RpcKernal.php';
-
-new \Group\Plugin\Rpc\Hprose();
-$kernal = new RpcKernal('tcp');
-$kernal->init();
